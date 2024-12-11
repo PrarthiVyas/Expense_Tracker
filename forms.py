409d41dcf7d5b3051a2bb2871854bda1fbbc8b0c
Prarthi Vyas <73277430+PrarthiVyas@users.noreply.github.com>
2024-12-11 20:36:57 +0530

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField,DateField,FloatField,StringField,SelectField
+from wtforms.validators import DataRequired
+
+
+choiceType=['Expense','Income']
+class expenseForm(FlaskForm):
+    expense=SelectField("Expense/Income",choices=choiceType)
+    amount=FloatField("Amount",validators=[DataRequired()])
+    date=DateField("Date",validators=[DataRequired()])
+    description=StringField("Description",validators=[DataRequired()])
+    category=SelectField(label='State', choices=['Bills', 'Food', 'Shopping','Transport','Medical','Rent','Money Transfer'])
+    submit = SubmitField("Save")
+
+
